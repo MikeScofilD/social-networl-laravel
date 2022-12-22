74f12e0fc27bb1b3f567e57665619b2616d1b643
Artem <38011337+MikeScofilD@users.noreply.github.com>
2022-12-22 18:16:48 +0200

    created friend invite

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,7 +3,18 @@
 @section('content')
     <div class="row">
         <div class="col-lg-6">
-              @include('user.partials.user_block')
+            @include('user.partials.user_block')
+        </div>
+        <div class="col-lg-4 col-lg-offset-3">
+            <h3>{{ $user->getFirstNameOrUserName() }} Друзья: </h3>
+
+            @if (!$user->friends()->count())
+                <h3>{{ $user->getFirstNameOrUserName() }} Нет друзей.</h3>
+            @else
+                @foreach ($user->friends() as $user)
+                    @include('user.partials.user_block')
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
