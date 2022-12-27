41592bfed3c7a62613e24edda702b3217032e49a
Artem <38011337+MikeScofilD@users.noreply.github.com>
2022-12-27 19:18:30 +0200

    show notes on the wall

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Status;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-   public function index()
-   {
+    public function index()
+    {
         if (Auth::check()) {
-            return view('timeline.index');
+            $statuses = Status::where(function ($query) {
+                return $query->where('user_id', Auth::user()->id)
+                    ->orWhereIn('user_id', Auth::user()->friends()->pluck('id'));
+            })->orderBy('created_at', 'desc')->paginate(1);
+
+            return view('timeline.index', compact('statuses'));
         }
         return view('home');
-   }
+    }
 }
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -20,9 +20,37 @@
             </form>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-5">
-
+    <div class="row mt-3">
+        <div class="col-lg-6">
+            @if (!$statuses->count())
+                <p>Нет ни одной записи</p>
+            @else
+                @foreach ($statuses as $status)
+                    <div class="media">
+                        {{-- {{dd($status->user->username)}} --}}
+                        <a href="{{route('profile.index', ['username' => $status->user->username])}}" class="mr-3"><img class="media-object rounded avatar" src="{{$status->user->getAvatarUrl()}}" alt="{{$status->user->getNameOrUserName()}}"></a>
+                        <div class="media-body">
+                            <h4><a href="{{route('profile.index', ['username' => $status->user->username])}}">{{$status->user->getNameOrUserName()}}</a></h4>
+                            <p>{{$status->body}}</p>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">{{$status->created_at->diffForHumans()}}</li>
+                                <li class="list-inline-item"><a href="">Лайк</a></li>
+                                <li class="list-inline-item">10 лайков</li>
+                            </ul>
+                            <form action="" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <textarea name="status" id="" cols="10" rows="2" class="form-control"
+                                        placeholder="Прокоментировать" rows="3"></textarea>
+                                </div>
+                                <input type="submit" class="btn btn-primary btn-sm" value="Ответить" name=""
+                                    id="">
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+                {{$statuses->links()}}
+            @endif
         </div>
     </div>
 @endsection
